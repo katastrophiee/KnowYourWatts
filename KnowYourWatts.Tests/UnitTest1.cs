fb9de17ed54764820a289b73955277457b14a908
Kaytlen Tommis- Birkett <Kaytlen.Tommis-Birkett@coxautoinc.com>
2024-09-20 14:17:53 +0100

    Add project files.

---
+++ KnowYourWatts.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace KnowYourWatts.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
