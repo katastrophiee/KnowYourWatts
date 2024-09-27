7388aef30e7d442cbd141f910f7c33e850ea628e
Will <122457153+W1llco@users.noreply.github.com>
2024-09-27 16:49:31 +0100

    Bootstrap working

--- knowyourwatts.clientui/src/App.js
+++ knowyourwatts.clientui/src/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 function App() {
    useEffect(() => {
@@ -22,6 +23,7 @@ function App() {
                 <div id="time"></div>
             </header>
             <div>
+            <nav>
                 <ul class="nav nav-tabs" id="myTab" role="tablist">
                     <li class="nav-item" role="presentation">
                         <button class="nav-link active" id="current-tab" data-bs-toggle="tab" data-bs-target="#current-tab-pane" type="button" role="tab" aria-controls="current-tab-pane" aria-selected="true">Current Usages</button>
@@ -33,12 +35,14 @@ function App() {
                         <button class="nav-link" id="weekly-tab" data-bs-toggle="tab" data-bs-target="#weekly-tab-pane" type="button" role="tab" aria-controls="weekly-tab-pane" aria-selected="false">Weekly Usage</button>
                     </li>
                 </ul>
+            </nav>
                 <div class="tab-content" id="myTabContent">
                     <div class="tab-pane fade show active" id="current-tab-pane" role="tabpanel" aria-labelledby="current-tab" tabindex="0">.1.</div>
                     <div class="tab-pane fade" id="todays-tab-pane" role="tabpanel" aria-labelledby="todays-tab" tabindex="0">.2.</div>
                     <div class="tab-pane fade" id="weekly-tab-pane" role="tabpanel" aria-labelledby="weekly-tab" tabindex="0">.3.</div>
                 </div>
             </div>
+
 
         </div>
           
