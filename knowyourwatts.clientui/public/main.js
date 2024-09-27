5a47bd5a8263ab1a7a303c01a23f15b17187fa80
Will <122457153+W1llco@users.noreply.github.com>
2024-09-27 16:34:20 +0100

    Bootstrap

--- knowyourwatts.clientui/public/main.js
+++ knowyourwatts.clientui/public/main.js
@@ -5,8 +5,8 @@ const path = require('node:path')
 function createWindow () {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 600,
+    height: 400,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
--- knowyourwatts.clientui/src/App.js
+++ knowyourwatts.clientui/src/App.js
@@ -1,25 +1,49 @@
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   useEffect(() => {
+        function displayTime() {
+            const now = new Date();
+            const hours = String(now.getHours()).padStart(2, '0');
+            const minutes = String(now.getMinutes()).padStart(2, '0');
+            document.getElementById('time').textContent = hours + ':' + minutes;
+        }
+        displayTime();
+        
+        const intervalId = setInterval(displayTime, 60000);
+        
+        return () => clearInterval(intervalId);
+    }, []); 
+    return (
+        <div className="App">
+            <header className="App-header">
+                <div id="time"></div>
+            </header>
+            <div>
+                <ul class="nav nav-tabs" id="myTab" role="tablist">
+                    <li class="nav-item" role="presentation">
+                        <button class="nav-link active" id="current-tab" data-bs-toggle="tab" data-bs-target="#current-tab-pane" type="button" role="tab" aria-controls="current-tab-pane" aria-selected="true">Current Usages</button>
+                    </li>
+                    <li class="nav-item" role="presentation">
+                        <button class="nav-link" id="todays-tab" data-bs-toggle="tab" data-bs-target="#todays-tab-pane" type="button" role="tab" aria-controls="todays-tab-pane" aria-selected="false">Today's Usage</button>
+                    </li>
+                    <li class="nav-item" role="presentation">
+                        <button class="nav-link" id="weekly-tab" data-bs-toggle="tab" data-bs-target="#weekly-tab-pane" type="button" role="tab" aria-controls="weekly-tab-pane" aria-selected="false">Weekly Usage</button>
+                    </li>
+                </ul>
+                <div class="tab-content" id="myTabContent">
+                    <div class="tab-pane fade show active" id="current-tab-pane" role="tabpanel" aria-labelledby="current-tab" tabindex="0">.1.</div>
+                    <div class="tab-pane fade" id="todays-tab-pane" role="tabpanel" aria-labelledby="todays-tab" tabindex="0">.2.</div>
+                    <div class="tab-pane fade" id="weekly-tab-pane" role="tabpanel" aria-labelledby="weekly-tab" tabindex="0">.3.</div>
+                </div>
+            </div>
+
+        </div>
+          
+    );
 }
 
 export default App;
+
