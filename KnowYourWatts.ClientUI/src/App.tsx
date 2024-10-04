f866efcda6963302f082fe900eeb89a9919af87b
Will <122457153+W1llco@users.noreply.github.com>
2024-10-04 19:14:38 +0100

    Style

--- KnowYourWatts.ClientUI/src/App.tsx
+++ KnowYourWatts.ClientUI/src/App.tsx
@@ -3,11 +3,11 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import { MonitorData } from "./MonitorData";
-import { FlexContainer } from "./StyledComponents.tsx";
-
+import * as Styled from "./StyledComponents.tsx";
 function App() {
     const [time, setTime] = useState<string>();
     const [homePageData, setHomePageData] = useState<MonitorData>();
+    const [activeTab, setToggleState] = useState(1);
 
     useEffect(() => {
 
@@ -27,49 +27,50 @@ function App() {
 
     return (
         <div className="App">
-            <FlexContainer>
-                Look a styled component!
-            </FlexContainer>
-            <header className="App-header">
-                <div>{time}</div>
-            </header>
+            <Styled.AppHeader>
+                {time}
+            </Styled.AppHeader>
             <div className="p-2">
-                <ul className="nav nav-tabs nav-justified py-1" id="myTab" role="tablist">
-                    <li className="nav-item pe-1" role="presentation">
-                        <button className="nav-link active" id="current-tab" data-bs-toggle="tab" data-bs-target="#current-tab-pane" type="button" role="tab" aria-controls="current-tab-pane" aria-selected="true">Current Usages</button>
-                    </li>
-                    <li className="nav-item pe-1" role="presentation">
-                        <button className="nav-link" id="todays-tab" data-bs-toggle="tab" data-bs-target="#todays-tab-pane" type="button" role="tab" aria-controls="todays-tab-pane" aria-selected="false">Today's Usage</button>
-                    </li>
-                    <li className="nav-item" role="presentation">
-                        <button className="nav-link" id="weekly-tab" data-bs-toggle="tab" data-bs-target="#weekly-tab-pane" type="button" role="tab" aria-controls="weekly-tab-pane" aria-selected="false">Weekly Usage</button>
-                    </li>
-                </ul>
-                <div className="tab-content" id="myTabContent">
-                    <div className="tab-pane fade show active py-4" id="current-tab-pane" role="tabpanel" aria-labelledby="current-tab">
-                        <div className="container row">
-                            <p className="col-10 align-top fs-4 fw-bold px-4">Usage:</p>
-                            <div className="col-2 text-center"><svg className="icon"xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path fill="#FFD43B" d="M0 256L28.5 28c2-16 15.6-28 31.8-28H228.9c15 0 27.1 12.1 27.1 27.1c0 3.2-.6 6.5-1.7 9.5L208 160H347.3c20.2 0 36.7 16.4 36.7 36.7c0 7.4-2.2 14.6-6.4 20.7l-192.2 281c-5.9 8.6-15.6 13.7-25.9 13.7h-2.9c-15.7 0-28.5-12.8-28.5-28.5c0-2.3 .3-4.6 .9-6.9L176 288H32c-17.7 0-32-14.3-32-32z" /></svg></div>
-                        </div>
-                        <p className="px-3">&pound;1.80</p>
-                        <p className="px-3">18KW</p>
-                    </div>
-                    <div className="tab-pane fade py-4" id="todays-tab-pane" role="tabpanel" aria-labelledby="todays-tab">
-                        <div className="container row">
-                            <p className="col-10 align-top fs-4 fw-bold px-4">Usage:</p>
-                            <div className="col-2 text-center"><svg className="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path fill="#FFD43B" d="M0 256L28.5 28c2-16 15.6-28 31.8-28H228.9c15 0 27.1 12.1 27.1 27.1c0 3.2-.6 6.5-1.7 9.5L208 160H347.3c20.2 0 36.7 16.4 36.7 36.7c0 7.4-2.2 14.6-6.4 20.7l-192.2 281c-5.9 8.6-15.6 13.7-25.9 13.7h-2.9c-15.7 0-28.5-12.8-28.5-28.5c0-2.3 .3-4.6 .9-6.9L176 288H32c-17.7 0-32-14.3-32-32z" /></svg></div>
-                        </div>
-                        <p className="px-3">&pound;1.80</p>
-                        <p className="px-3">18KW</p>
-                    </div>
-                    <div className="tab-pane fade py-4" id="weekly-tab-pane" role="tabpanel" aria-labelledby="weekly-tab">
-                        <div className="container row">
-                            <p className="col-10 align-top fs-4 fw-bold px-4">Usage:</p>
-                            <div className="col-2 text-center"><svg className="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path fill="#FFD43B" d="M0 256L28.5 28c2-16 15.6-28 31.8-28H228.9c15 0 27.1 12.1 27.1 27.1c0 3.2-.6 6.5-1.7 9.5L208 160H347.3c20.2 0 36.7 16.4 36.7 36.7c0 7.4-2.2 14.6-6.4 20.7l-192.2 281c-5.9 8.6-15.6 13.7-25.9 13.7h-2.9c-15.7 0-28.5-12.8-28.5-28.5c0-2.3 .3-4.6 .9-6.9L176 288H32c-17.7 0-32-14.3-32-32z" /></svg></div>
-                        </div>
-                        <p className="px-3">&pound;1.80</p>
-                        <p className="px-3">18KW</p>
-                    </div>
+                <Styled.NavTabs>
+                    <Styled.NavItem>
+                        <button className={activeTab === 1 ? "nav-link active" : "nav-link"} onClick={() => setToggleState(1)}>tab 1</button>
+                    </Styled.NavItem>
+                    <Styled.NavItem>
+                        <button className={activeTab === 2 ? "nav-link active" : "nav-link"} onClick={() => setToggleState(2)}>tab 2</button>
+                    </Styled.NavItem>
+                    <Styled.NavItem>
+                        <button className={activeTab === 3 ? "nav-link active" : "nav-link"} onClick={() => setToggleState(3)}>tab 3</button>
+                    </Styled.NavItem>
+                </Styled.NavTabs>
+
+                <div className="content"> 
+                    {activeTab === 1 && 
+                        <Styled.TabPane>
+                            <div className="container row">
+                                <Styled.Usage>Usage:</Styled.Usage>
+                                <div className="col-2 text-center"><svg className="icon"xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path fill="#FFD43B" d="M0 256L28.5 28c2-16 15.6-28 31.8-28H228.9c15 0 27.1 12.1 27.1 27.1c0 3.2-.6 6.5-1.7 9.5L208 160H347.3c20.2 0 36.7 16.4 36.7 36.7c0 7.4-2.2 14.6-6.4 20.7l-192.2 281c-5.9 8.6-15.6 13.7-25.9 13.7h-2.9c-15.7 0-28.5-12.8-28.5-28.5c0-2.3 .3-4.6 .9-6.9L176 288H32c-17.7 0-32-14.3-32-32z" /></svg></div>                            
+                            </div>
+                            <p className="px-3">&pound;1.80</p>
+                            <p className="px-3">18KW</p>
+                        </Styled.TabPane>}
+                    {activeTab === 2 && 
+                        <Styled.TabPane>
+                            <div className="container row">
+                                <Styled.Usage>Usage:</Styled.Usage>   
+                                <div className="col-2 text-center"><svg className="icon"xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path fill="#FFD43B" d="M0 256L28.5 28c2-16 15.6-28 31.8-28H228.9c15 0 27.1 12.1 27.1 27.1c0 3.2-.6 6.5-1.7 9.5L208 160H347.3c20.2 0 36.7 16.4 36.7 36.7c0 7.4-2.2 14.6-6.4 20.7l-192.2 281c-5.9 8.6-15.6 13.7-25.9 13.7h-2.9c-15.7 0-28.5-12.8-28.5-28.5c0-2.3 .3-4.6 .9-6.9L176 288H32c-17.7 0-32-14.3-32-32z" /></svg></div>
+                            </div>
+                            <p className="px-3">&pound;1.80</p>
+                            <p className="px-3">18KW</p>
+                        </Styled.TabPane>}
+                    {activeTab === 3 && 
+                        <Styled.TabPane>
+                            <div className="container row">
+                                <Styled.Usage>Usage:</Styled.Usage>
+                                <div className="col-2">⚡</div>
+                            </div>
+                            <p className="px-3">&pound;1.80</p>
+                            <p className="px-3">18KW</p>
+                        </Styled.TabPane>}
                 </div>
             </div>
         </div>
--- KnowYourWatts.ClientUI/src/StyledComponents.tsx
+++ KnowYourWatts.ClientUI/src/StyledComponents.tsx
@@ -1,5 +1,32 @@
 import styled from 'styled-components';
 
-export const FlexContainer = styled.div`
-  display: flex;
+export const AppHeader = styled.header`
+    text-align: center;
+    vertical-align: central;
+    padding: 2px;
+    background-color: #323232;
+    color: #fff;
+    font-size: 24px;
+`;
+
+export const TabPane = styled.div.attrs((props) => ({
+    className: 'py-4 '
+}))`
+    background-color: #323232;
+    color: #ECECEC;
+    border-radius: 10px;
+`;
+
+export const Usage = styled.div.attrs((props) => ({
+    className: 'col-10 align-top fs-4 fw-bold px-4'
+}))``;
+
+export const NavItem = styled.li.attrs((props) => ({
+    className: 'nav-item pe-1'
+}))``;
+
+export const NavTabs = styled.ul.attrs((props) => ({
+    className: 'nav nav-tabs nav-justified py-1'
+}))`
+border-color: transparent !important;
 `;
