72c8f4387e9dee864de658fc615280785e1e1b4a
kaytl <kaytl@DESKTOP-TI4LR7A>
2024-10-07 21:01:40 +0100

    Change logic and setup for request type handling

---
+++ KnowYourWatts.Server/Server.cs
@@ -0,0 +1,54 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Net;
+using System.Net.Sockets;
+
+namespace KnowYourWatts.Server;
+
+[method: SetsRequiredMembers]
+public class Server(IPHostEntry host, IPAddress ipAddress, IPEndPoint localEndPoint)
+{
+    private readonly IPHostEntry Host = host ?? throw new ArgumentNullException(nameof(host));
+    private readonly IPAddress IpAddress = ipAddress ?? throw new ArgumentNullException(nameof(ipAddress));
+    private readonly IPEndPoint LocalEndPoint = localEndPoint ?? throw new ArgumentNullException(nameof(localEndPoint));
+    private Socket Listener;
+    private bool RunServer;
+
+    public void Start()
+    {
+        try
+        {
+            Listener = new Socket(IpAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            Listener.Bind(LocalEndPoint);
+            Listener.Listen(30);
+            RunServer = true;
+
+            Console.WriteLine($"Server started at {LocalEndPoint.Address}:{LocalEndPoint.Port}");
+
+            while (RunServer)
+            {
+                if (Listener.Poll(1000, SelectMode.SelectRead))
+                {
+                    var handler = Listener.Accept();
+                    ThreadPool.QueueUserWorkItem(state => HandleClient(handler));
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("An error occured when starting the server: " + ex.ToString());
+        }
+    }
+
+    private static void HandleClient(Socket handler)
+    {
+        var connectionHandler = new ConnectionHandler(handler);
+        connectionHandler.HandleConnection();
+    }
+
+    public void Stop()
+    {
+        RunServer = false;
+        Listener.Close();
+        Console.WriteLine("Server stopped.");
+    }
+}
