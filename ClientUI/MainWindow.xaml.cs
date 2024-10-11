7e181fe64d035433ae2f9c632d3542d7ba7edd49
meghn <meghn@LAPTOP-EM5DDA9S>
2024-10-11 17:12:20 +0100

    client side code. partially done. was getting a bit confused. will continue to finish later today.

--- ClientUI/MainWindow.xaml.cs
+++ ClientUI/MainWindow.xaml.cs
@@ -1,4 +1,9 @@
-﻿using System.Text;
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,6 +13,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Newtonsoft.Json;
+using KnowYourWatts.DTO.Requests;
+using KnowYourWatts.DTO.Enums;
 
 namespace ClientUI
 {
@@ -16,9 +24,52 @@ namespace ClientUI
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Socket clientSocket;
         public MainWindow()
         {
             InitializeComponent();
+        }
+        private void ConnectToServer()
+        {
+            try
+            {
+                var host = Dns.GetHostEntry("localhost");
+                var ipAddress = host.AddressList[0];
+                var remoteEndPoint = new IPEndPoint(ipAddress, 11000);
+
+                clientSocket = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+                clientSocket.Connect(remoteEndPoint);
+                
+            }
+            catch (Exception ex)
+            {
+               MessageBox.Show(ex.Message);
+            }
+        }
+        private void SendCurrentUsage(object sender, RoutedEventArgs e)
+        {
+            
+        }
+        private void SendRequest(RequestType requestType,CurrentUsageRequest currentUsageRequest)
+        {
+            if(clientSocket == null || !clientSocket.Connected)
+            {
+                //Display error message
+                return;
+            }
+            try
+            {
+                var request = new ServerRequest
+                {
+                    Type = requestType,
+                    Data = JsonConvert.SerializeObject(currentUsageRequest)
+                };
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
