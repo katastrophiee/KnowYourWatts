40ba5b46466cbfb0f4654238e99f08df14a290eb
Will <122457153+W1llco@users.noreply.github.com>
2024-10-11 18:36:18 +0100

    Frontend style

--- ClientUI/MainWindow.xaml.cs
+++ ClientUI/MainWindow.xaml.cs
@@ -8,17 +8,36 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace ClientUI
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window  
     {
+        private DispatcherTimer timer;
+
         public MainWindow()
         {
             InitializeComponent();
+            StartClock();
+        }
+
+        private void StartClock()
+        {
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromMinutes(1);
+            timer.Tick += Timer_Tick;
+            timer.Start();  
+
+            TimeDisplay.Text = DateTime.Now.ToString("HH:mm");
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            TimeDisplay.Text = DateTime.Now.ToString("HH:mm");
         }
     }
 }
