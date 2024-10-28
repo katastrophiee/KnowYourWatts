741b5ffe033df35133af1094312de27986d991ce
Will <122457153+W1llco@users.noreply.github.com>
2024-10-28 18:32:08 +0000

    Removal of client ui
    Removal of client ui

--- KnowYourWatts.ClientUI.DTO/Requests/DailyUsageRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿using KnowYourWatts.ClientUI.DTO.Enums;
-
-namespace KnowYourWatts.ClientUI.DTO.Requests;
-
-public class DailyUsageRequest
-{
-    public TariffType TariffType { get; set; }
-    public decimal CurrentReading { get; set; }
-}
--- KnowYourWatts.ClientUI.DTO/Requests/WeeklyUsageRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿using KnowYourWatts.ClientUI.DTO.Enums;
-
-namespace KnowYourWatts.ClientUI.DTO.Requests;
-
-public class WeeklyUsageRequest
-{
-    public TariffType TariffType { get; set; }
-    public decimal CurrentReading { get; set; }
-}
