41bcdc4e0ee6b54bb428c0b89154555f33130b21
Kaytlen Tommis-Birkett <Kaytlen.Tommis-Birkett@coxautoinc.com>
2024-11-04 12:54:00 +0000

    Add MAUI

---
+++ KnowYourWatts.ClientUI/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace KnowYourWatts.ClientUI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
