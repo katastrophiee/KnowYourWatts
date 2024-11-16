2e2aef3882c3290b7927f1964283a0cfe291a26e
grchamberlain <georgia.chamberlain@hotmail.com>
2024-11-16 16:50:28 +0000

    Edited ServerRequest to contain encrypted MPAN

--- KnowYourWatts.DTO/Requests/ServerRequest.cs
+++ KnowYourWatts.DTO/Requests/ServerRequest.cs
@@ -7,6 +7,8 @@ public class ServerRequest
     //A Meter Point Administration Number - a unique 13-digit number that identifies a smart meter
     public string Mpan { get; set; }
 
+    public byte[] encryptedMPAN {  get; set; }
+
     public RequestType RequestType { get; set; }
 
     public string Data { get; set; }
